26edaa6819605a08992e5b4a4d5a72efc950e0c4
ehp2021 <emmahpaik@gmail.com>
2021-11-19 14:53:25 -0600

    santiago's fault

---
+++ client/src/components/Grid/index.tsx
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom";
+
+//components
+import Loader from "../Loader/index.js";
+import HeadingTitle from "../HeadingTitle/index.js";
+
+//styles
+import { Wrapper, Content, Card, Image } from "./Grid.styles.js";
+
+type GridProp = {
+  playlists: []
+}
+
+const Grid = ({playlists, source}) => {
+  console.log(typeof(playlists), "playlists")
+return (
+  
+  <Wrapper>
+{playlists && playlists.length ? (
+  <>
+      <HeadingTitle title={`Top ${source}s`}/>
+
+  <Content>
+        {playlists.map((playlist, i) => (
+          <Card key={i}>
+            <Link className="playlist_name" to={`/${source}/${playlist.id}`}>
+              {playlist.images.length && playlist.images[0] && (
+                <div >
+                  <Image src={playlist.images[0].url} alt={playlist.name} />
+                </div>
+              )}
+              <h3 >{playlist.name}</h3>
+            </Link>
+          </Card>
+        ))}
+  </Content>
+  </>
+    ) : ( <Loader/> )}
+
+  </Wrapper>
+    
+)
+};
+
+export default Grid;
